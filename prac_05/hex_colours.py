66a5fb7e452daa8a3640592521d52107dfe4c1a5
SamShepp <sam_shepp@hotmail.com>
2018-09-06 13:29:34 +1000

    Fix capital letter

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,7 +3,7 @@ CP1404 Practical
 Hexadecimal colour lookup
 """
 
-COLOUR_CODES = {"AliceBlue": "#f0f8ff", "blue1": "#0000ff", "BlueViolet": "#8a2be2",
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "Blue1": "#0000ff", "BlueViolet": "#8a2be2",
                 "Brown": "#a52a2a", "CadetBlue1": "#98f5ff", "Chartreuse1": "#7fff00",
                 "Chocolate": "#d2691e", "Coral": "#ff7f50", "CornFlowerBlue": "#6495ed",
                 "Cyan1": "#00ffff"}
